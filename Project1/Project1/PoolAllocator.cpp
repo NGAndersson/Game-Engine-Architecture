236b25a0088776b527feb9b067f7f1109a407e47
yorkhawke <yorkhawke@DESKTOP-T7V966J>
2016-09-07 16:34:02 +0200

    poolfix

---
+++ Project1/Project1/PoolAllocator.cpp
@@ -0,0 +1,17 @@
+#include "PoolAllocator.h"
+
+
+void PoolAllocator::setupPool(int size)
+{
+
+}
+
+void PoolAllocator::remove(void* ptr)
+{
+
+}
+
+void* PoolAllocator::getSome(void)
+{
+
+}
--- Project1/Project1/PoolAllocator.h
+++ Project1/Project1/PoolAllocator.h
@@ -1,1 +1,32 @@
-#pragma once
+#ifndef POOLALLOCATOR_H
+#define POOLALLOCATOR_H
+#include <stdio.h>
+#include <string.h>
+#include "Allocator.h"
+
+using namespace std; 
+
+class PoolAllocator : public Allocator
+{
+//Attributes
+private:
+	void* _memoryBlock;
+
+
+public:
+
+
+//Methods
+private:
+
+
+public:
+	void setupPool(int size); //Gets chunk of memory and creates a linked list
+	void remove(void* ptr); //Used for removing a block
+	void* getSome(void); //Returns block of memory
+
+};
+
+
+#endif
+
