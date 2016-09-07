b7f886c4566e9703dc4185c3ec3020274c2d5f21
yorkhawke <yorkhawke@DESKTOP-T7V966J>
2016-09-07 16:59:20 +0200

    Fixed oscars MISTAKE

---
+++ Project1/Project1/Source/PoolAllocator.cpp
@@ -0,0 +1,17 @@
+#include "../Header/PoolAllocator.h"
+
+
+void PoolAllocator::setupPool(int size)
+{
+
+}
+
+void PoolAllocator::remove(void* ptr)
+{
+
+}
+
+void* PoolAllocator::getSome(void)
+{
+
+}
