41135bcde9e6011e217514939d6a3944e813198c
yorkhawke <yorkhawke@DESKTOP-5BSJ1E6>
2016-09-27 15:42:08 +0200

    AddedCompressorClass

---
+++ Project1/Assignment 2/Compressor.cpp
@@ -0,0 +1,25 @@
+#include "Compressor.h"
+
+Compressor::Compressor()
+{
+
+
+}
+
+Compressor::~Compressor()
+{
+
+
+}
+
+void Compressor::decompress(char* path)
+{
+
+
+}
+
+void Compressor::compress()
+{
+
+
+}
