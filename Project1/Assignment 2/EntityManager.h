365ed68b5ad886eee66330f1003d6f3a485f17f8
SwedishSock <radditz770@gmail.com>
2016-10-17 15:39:07 +0200

    Commit to branch for home usage

--- Project1/Assignment 2/EntityManager.h
+++ Project1/Assignment 2/EntityManager.h
@@ -5,9 +5,11 @@
 #include "Renderer.h"
 #include "ShaderHandler.h"
 #include "ModelHandler.h"
+#include "Loader.h"
 #include <d3d11.h>
 #include <random>
 #include <memory>
+#include "Camera.h"
 
 #pragma comment (lib, "d3d11.lib")
 #pragma comment (lib, "d3dcompiler.lib")
@@ -30,14 +32,24 @@ private:
 	//Vectors with all the different types of entities
 	ShaderHandler* m_shaderLoad;
 	ModelHandler* m_modelHandler;
-	Entity* m_entity;
 
+	Loader m_loader;
+	void* m_tempAsset;
+	char* m_objAsset; //should be a pointer of type ModelHandler or OBJ or whatever depending on what format we choose to do it in
+	char* m_mtlAsset;
+	char* m_vAsset;
+	char* m_gAsset;
+	char* m_pAsset;
+	int LoD = 0;
+	XMFLOAT3 m_camPos;
+	XMFLOAT3 m_entityPos;
+	Camera m_cam;
 	Renderer* m_renderer;
 	ID3D11Device* m_device;
 	ID3D11DeviceContext* m_deviceContext;
 	XMMATRIX m_xmrot;
 public:
-
+	Entity* m_entity;
 };
 
 #endif // !_ENTITYMANAGER_H
