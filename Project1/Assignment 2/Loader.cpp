4d657cf94c4264192afbf63c679766f18af11316
BTH <BTH@DESKTOP-EQNENPF>
2016-10-17 15:22:52 +0200

    Added pinning&referencecount and made changes to registry layout

--- Project1/Assignment 2/Loader.cpp
+++ Project1/Assignment 2/Loader.cpp
@@ -18,46 +18,83 @@ void * Loader::Get(std::string guid)
 	mtxLock.lock();
 	if (registry.find(guid) != registry.end())	//guid already exists in registry
 	{
-		if (registry[guid] != nullptr)	//Already being worked on by another thread and will be inserted soon
+		if (registry[guid].data != nullptr)	//Already being worked on by another thread and will be inserted soon
 			return nullptr;
 		else
-			return registry[guid];		//Asset already exists in registry and has a proper pointer. Return the address
+			return reinterpret_cast<void*>(registry[guid].data);		//Asset already exists in registry and has a proper pointer. Return the address
 	}
 	else	//Guid is not yet in registry
 	{	//Find out if there's such a guid in our packages
 		if (guid.substr(0, 3) == "los")			//Asset is in our .los custom package
+		{
 			FindOffsetCustom(guid, losByteOffset, losByteSize);
+			usedMemory += losByteSize;
+			registry[guid].size = losByteSize;
+		}
 		else									//Asset is in a .zip package
-			filePath = FindPathZip(guid);
+			filePath = FindPathZip(guid);		//Add to usedMemory in function
 		//Will crash deliberately if FindPath functions can't find the asset
 
-		registry[guid] = nullptr;		//If the asset exists but hasn't been loaded set it as nullptr temporarily so other threads can see that it's being loaded
+		registry[guid].data = nullptr;		//If the asset exists but hasn't been loaded set it as nullptr temporarily so other threads can see that it's being loaded
 	}
 	mtxLock.unlock();
 
 
 	//If we've gotten this far, we can begin decompressing and processing the file
 	std::string readerType = guid.substr(0, guid.find_first_of('.'));
-	std::istringstream fileStream;
+	
 	//Send filepath to correct package handler here
 	if (readerType == "zip")
-		registry[guid] = decompressor.decompress("test.zip", filePath);
+		registry[guid].data = decompressor.decompress("test.zip", filePath);
 
 	if (readerType == "los")
-		registry[guid] = losReader.read("test.los", losByteOffset, losByteSize);
+		registry[guid].data = losReader.read("test.los", losByteOffset, losByteSize);
 
-
-	return retPtr;
+	registry[guid].referenceCount++;
+	return reinterpret_cast<void*>(registry[guid].data);
 }
 
 void Loader::Free(std::string guid)
 {
-	std::unordered_map<std::string, char*>::const_iterator it = registry.find(guid);
-	if (it == registry.end()) return;
-	else
-		delete[] it->second;
-	registry.erase(it->first);
+	if (registry.find(guid) != registry.end())
+	{
+		registry[guid].referenceCount--;
+	}
 }
+
+void Loader::Free()
+{
+	while (usedMemory > maxMemory)
+	{
+		for (auto i : registry)
+		{
+			if (!i.second.pinned && i.second.referenceCount <= 0)
+			{
+				delete[] i.second.data;
+				usedMemory -= i.second.size;
+				registry.erase(i.first);
+				break; //Break the for loop so we can get another iterator, as the unordered_map has been altered
+			}
+		}
+	}
+}
+
+void Loader::Pin(std::string guid)
+{
+	if (registry.find(guid) != registry.end())
+	{
+		registry[guid].pinned = true;
+	}
+}
+
+void Loader::Unpin(std::string guid)
+{
+	if (registry.find(guid) != registry.end())
+	{
+		registry[guid].pinned = false;
+	}
+}
+
 
 std::string Loader::FindPathZip(std::string guid)
 {
@@ -71,8 +108,15 @@ std::string Loader::FindPathZip(std::string guid)
 		std::getline(fileTable, line, ' ');		//Look for guid in table
 		if (line == guid)						//If found
 		{
-			std::getline(fileTable, line);		//Get filepath of guid
+			std::getline(fileTable, line, ' ');		//Get filepath of guid
 			fileTable.close();
+			std::string size;
+			std::getline(fileTable, size);
+			usedMemory += stoi(size);
+			if (usedMemory > maxMemory)
+			{
+				registry[guid].pinned = true; Free(); registry[guid].pinned = false;	//Temporarily pin so we don't remove the entry we're trying to load in order to free up the space to load it
+			}
 			found = true;
 		}
 		else std::getline(fileTable, line);		//go to next line
@@ -105,6 +149,12 @@ void Loader::FindOffsetCustom(std::string guid, int& offset, int& size)
 			offset = stoi(line);
 			std::getline(file, line);
 			size = stoi(line);
+			usedMemory += size;
+			registry[guid].size = size;
+			if (usedMemory > maxMemory)
+			{
+				registry[guid].pinned = true; Free(); registry[guid].pinned = false;	//Temporarily pin so we don't remove the entry we're trying to load in order to free up the space to load it
+			}
 			file.close();
 			return;
 		}
--- Project1/Assignment 2/Loader.h
+++ Project1/Assignment 2/Loader.h
@@ -5,17 +5,27 @@
 #include "Decompressor.h"
 #include "LOSReader.h"
 
+struct RegistryEntry
+{
+	char* data;
+	int size = 0;
+	int referenceCount = 0;
+	bool pinned = false;
+};
+
 class Loader{
 public:
 	Loader();
 	void* Get(std::string guid);
 	void Free(std::string guid);
-
+	void Pin(std::string guid);
+	void Unpin(std::string guid);
 
 private:
+	void Free();
 	std::string FindPathZip(std::string guid);
 	void FindOffsetCustom(std::string guid, int& offset, int& size);
-	std::unordered_map<std::string, char*> registry;
+	std::unordered_map<std::string, RegistryEntry> registry;
 	int maxMemory;
 	int usedMemory;
 	std::mutex mtxLock;
